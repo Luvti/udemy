cfc95f278afcb7ff864d1cbeb72ef0d866a3d858
Luvti <i@luvti.com>
2020-01-29 23:16:10 +0300

    Flutter [7] - categories

--- Flutter/meals/lib/main.dart
+++ Flutter/meals/lib/main.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
 
-import './categories_screen.dart';
-import './category_meals_screen.dart';
+import './screens/categories_screen.dart';
+import './screens/category_meals_screen.dart';
+import './screens/meal_datail_screen.dart';
 
 void main() => runApp(MyApp());
 
@@ -35,7 +36,18 @@ class MyApp extends StatelessWidget {
       routes: {
         '/': (ctx) =>  CategoriesScreen(),
         CategoryMealsScreen.routeName: (ctx) => CategoryMealsScreen(),
-      }
+        MealDetailScreen.routeName: (ctx) => MealDetailScreen(),
+      },
+      // onGenerateRoute: (settings) {
+      //   print(settings.arguments);
+      //   if (settings.name == MealDetailScreen.routeName) {
+      //   return MaterialPageRoute(builder: (ctx) => MealDetailScreen());
+      //   }
+      //   return MaterialPageRoute(builder: (ctx) => CategoriesScreen());
+      // },
+      onUnknownRoute: (settings) {
+        return MaterialPageRoute(builder: (ctx) => CategoriesScreen(),);
+      },
     );
   }
 }
--- Flutter/meals/lib/screens/category_meals_screen.dart
+++ Flutter/meals/lib/screens/category_meals_screen.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 
+import '../widgets/meal_item.dart';
+import '../dummy_data.dart';
+
 class CategoryMealsScreen extends StatelessWidget {
 
   static const routeName = '/category-meals';
@@ -15,14 +18,25 @@ class CategoryMealsScreen extends StatelessWidget {
     final routeArgs = ModalRoute.of(context).settings.arguments as Map<String, String>;
     final categoryId = routeArgs['id'];
     final categoryTitle = routeArgs['title'];
+    final categoryMeals = DUMMY_MEALS.where((meal) {
+      return meal.categories.contains(categoryId);
+    }).toList();
     return Scaffold(
       appBar: AppBar(
         title: Text(categoryTitle),
       ),
       body: ListView.builder(
         itemBuilder: (ctx, index) {
-          return null;
+          return MealItem(
+            id: categoryMeals[index].id,
+            title: categoryMeals[index].title,
+            imageUrl: categoryMeals[index].imageUrl,
+            duration: categoryMeals[index].duration,
+            complexity: categoryMeals[index].complexity,
+            affordability: categoryMeals[index].affordability,
+            );
         },
+        itemCount: categoryMeals.length,
       ),
     );
   }
