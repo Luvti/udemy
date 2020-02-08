2e7ff8002ac1eb1b5e7c7236d9e08f013289f846
Luvti <i@luvti.com>
2020-02-08 14:50:59 +0300

    Flutter [8] - shop - widgets, pages

---
+++ Flutter/shop/lib/screens/product_detail_screen.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class ProductDetailsScreen extends StatelessWidget {
+
+  static const routePath = 'product-detail';
+  @override
+  Widget build(BuildContext context) {
+    final productId = ModalRoute.of(context).settings.arguments as String;
+    return Scaffold(
+      appBar: AppBar(title: Text('title'),),
+    );
+  }
+}
