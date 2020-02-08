4ce9e70ea4eff14434e009b0869ee69ed2b5e458
Luvti <i@luvti.com>
2020-02-08 15:40:27 +0300

    Flutter [8] - shop - providers

--- Flutter/shop/lib/screens/product_detail_screen.dart
+++ Flutter/shop/lib/screens/product_detail_screen.dart
@@ -1,13 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import '../providers/products.dart';
 
 class ProductDetailsScreen extends StatelessWidget {
-
   static const routePath = 'product-detail';
   @override
   Widget build(BuildContext context) {
     final productId = ModalRoute.of(context).settings.arguments as String;
+    final product = Provider.of<Products>(
+      context,
+      listen: false, // не вызывает Rebiuld
+    ).findById(productId);
     return Scaffold(
-      appBar: AppBar(title: Text('title'),),
+      appBar: AppBar(
+        title: Text(product.title),
+      ),
     );
   }
 }
