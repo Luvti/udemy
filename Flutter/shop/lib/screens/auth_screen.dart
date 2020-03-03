b116fdff823b92f87ccc2f4fe008bd180081b3f5
Luvti <i@luvti.com>
2020-03-03 16:00:09 +0300

    Flutter [12] - AnimatedContainer

--- Flutter/shop/lib/screens/auth_screen.dart
+++ Flutter/shop/lib/screens/auth_screen.dart
@@ -219,17 +219,19 @@ class _AuthCardState extends State<AuthCard>
         borderRadius: BorderRadius.circular(10.0),
       ),
       elevation: 8.0,
-      child: AnimatedBuilder(
-        animation: _heightAnimation,
-        builder: (ctx, ch) => Container(
-        // height: _authMode == AuthMode.Signup ? 320 : 260,
-        height: _heightAnimation.value.height,
+      child: // AnimatedBuilder(
+      //   animation: _heightAnimation,
+      //   builder: (ctx, ch) => 
+      AnimatedContainer(
+        duration: Duration(milliseconds: 300),
+        curve: Curves.easeInOut,
+        height: _authMode == AuthMode.Signup ? 320 : 260,
+        // height: _heightAnimation.value.height,
         constraints:
-            BoxConstraints(minHeight: _heightAnimation.value.height),
+            BoxConstraints(minHeight:  _authMode == AuthMode.Signup ? 320 : 260), // _heightAnimation.value.height),
         width: deviceSize.width * 0.75,
         padding: EdgeInsets.all(16.0),
-        child: ch
-      ),
+        // child: ch),
         child: Form(
           key: _formKey,
           child: SingleChildScrollView(
